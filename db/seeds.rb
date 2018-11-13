4a3516360486f94b50486355023b85f0a44b4043
github username <github email address>
2018-11-13 12:23:44 -0500

    added the dumy data to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,4 @@
-# Add seed data here. Seed your database with `rake db:seed`
+#require_relative '../app/models/product.rb'
 
-# Movie.create(title: "Wargames", release_date: 1983, director: "John Badham", lead: "Matthew Broderick", in_theaters: false)
-# create_table "products",
-#   t.string "name"
-#   t.float "price"
-#   t.integer "quantity"
-#   t.integer "category_id"
-# need to add following columns to products table in database
-  #gender
-  #size
-  #color
-Product.create(name: "Mens T-shirt size - L", price: 35.50, quantity: 30, category_id: 2 )
-create_table "categories", force: :cascade do |t|
-  t.string "name"
-end
 
-# create_table "categories"
-#   t.string "name"
-# end
-
-Catagory.create(name:"clothing")
-
-# create_table "customers"
-#   t.string "name"
-# end
-
-Customer.create(name: "John")
-#
-# create_table "orderproducts"
-#   t.integer "order_id"
-#   t.integer "product_id"
-# end
-#
-# create_table "orders"
-#   t.integer "customer_id"
-#   t.integer "orderproduct_id"
-#   t.float "payment"
-# end
+Product.create(Product.new_data)
