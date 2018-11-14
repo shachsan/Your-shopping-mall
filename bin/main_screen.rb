d2d8e5125d35930843a364152b3e4262edad20b6
Sanjay Sai <shachsan@hotmail.com>
2018-11-14 12:39:51 -0500

    minor change

---
+++ bin/main_screen.rb
@@ -0,0 +1,18 @@
+
+require "highline/import"
+
+
+puts "Welcome to your shopping mall"
+
+
+# begin
+#   puts
+#   loop do
+#     choose do |menu|
+#       menu.prompt = "Please select: "
+#       menu.choice("Order From our catelog") { customer_login_prompt() }
+#       menu.choice(:Adminstrator) { admin_login_prompt() }
+#       menu.choice(:Quit, "Exit program.") { exit }
+#     end
+#   end
+# end
